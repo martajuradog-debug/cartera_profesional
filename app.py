8fbd78b8963c95119c95de833846aaf2fb2b1f46
martajuradog-debug <martajuradog@gmail.com>
2026-05-27 20:26:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import streamlit as st
-import pandas as pd
+import yfinance as yf
 import plotly.express as px
 
 st.title("Mi cartera automática")
 
-df = pd.read_csv("datos.csv", index_col=0)
+# Acciones
+tickers = ["AAPL", "MSFT", "NVDA", "AMZN", "SPY"]
 
+# Descargar datos
+df = yf.download(tickers, start="2020-01-01")["Close"]
+
+# Crear gráfico
 fig = px.line(df, title="Precios históricos")
 
+# Mostrar gráfico
 st.plotly_chart(fig, use_container_width=True)
